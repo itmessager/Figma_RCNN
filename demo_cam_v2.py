e575f2f2bf677be8eedb971c335d91f37c250f33
itmessager <dagoo@qq.com>
2019-07-15 09:19:45 +0000

    modify

---
+++ demo_cam_v2.py
@@ -0,0 +1,128 @@
+import cv2
+import argparse
+import numpy as np
+import time
+
+from PIL import ImageDraw, Image
+from attributer.attributer import PersonAttrs, PersonBoxes
+from utils.viz_utils_en import draw_tracked_people
+
+
+def run(process_func, args, cam=None, video=None, image=None):
+    if cam:
+        # Read camera
+        cap = cv2.VideoCapture(0)
+    elif video:
+        # Read video
+        cap = cv2.VideoCapture(video)
+    elif image:
+        cap = cv2.VideoCapture(image)
+    else:
+        raise Exception("Either cam or video need to be specified as input")
+
+    # Initialize model
+    width, height = cap.get(3), cap.get(4)
+    print((width, height))
+    models = get_detector(args.load_ckpt, args.config)
+
+    frame_count = 0
+    while True:
+
+        grabbed, image_bgr = cap.read()
+
+        if not grabbed:
+            break
+
+        frame_count += 1
+        t = time.time()
+        img_to_show = process_func(models, image_bgr)
+        print("Process frame {} takes {}s".format(frame_count, time.time() - t))
+
+        if img_to_show is not None:
+            cv2.imshow('video', img_to_show)
+            if image:
+                cv2.waitKey(0)
+            elif video or cam:
+                k = cv2.waitKey(1)
+                if k == 27:  # Esc key to stop
+                    break
+
+
+def get_detector(weight_file, config):
+
+    from detection.tensorpacks.inference import AttributeDetector
+    from detection.config.config import config as cfg
+    if config:
+        cfg.update_args(config)
+    return AttributeDetector(weight_file)
+
+
+# use models to detect
+def process_detector_func(models, image_bgr):
+    # Perform detection
+    person_results = models.detect(image_bgr, rgb=False)
+
+    # # get the people's boxes,masks,scores,id
+    # people_boxes = [PersonBoxes(r) for r in person_results]
+    # # Calculate people's attributes
+    # people_attrs = [PersonAttrs(r) for r in person_results]
+    # Draw detection and tracking results
+    image_disp = draw_tracked_people(image_bgr, person_results)
+
+    # Draw attribute results
+    image_pil = Image.fromarray(cv2.cvtColor(image_disp, cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(image_pil)
+    # for trk, attr in zip(people_boxes, people_attrs):
+    #     draw_person_attributes(draw, attr, trk.body_box)
+
+    image_disp = cv2.cvtColor(np.array(image_pil), cv2.COLOR_RGB2BGR)
+
+    return image_disp
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--video',
+        type=str,
+        help="Run prediction on a given video. "
+             "This argument is the path to the input video file")
+    parser.add_argument(
+        '--image',
+        type=str,
+        help="Run prediction on a given image. "
+             "This argument is the path to the input image file")
+    parser.add_argument(
+        '--cam',
+        type=int,
+        help='Specify which camera to detect.')
+
+    parser.add_argument(
+        '--load_ckpt',
+        default='',
+        type=str,
+        help='Checkpoint of object detection model')
+    parser.add_argument(
+        '--config',
+        default='',
+        type=str,
+        help='Configurations of object detection model',
+        nargs='+'
+    )
+    parser.add_argument(
+        '--pretrain', action='store_true', help='Whether to use pretrained weights in conv models')
+    args = parser.parse_args()
+
+    run(process_detector_func, args, args.cam, args.video, args.image)
+
+'''
+--image
+/root/datasets/img-folder/1.jpg
+--cam
+0
+--load_ckpt
+/root/datasets/0601/checkpoint
+--config
+DATA.BASEDIR=/root/datasets/COCO/DIR
+
+'''
---
+++ utils/viz_utils_en.py
@@ -0,0 +1,139 @@
+import cv2
+from PIL import Image, ImageDraw
+import numpy as np
+from object_detection.utils.visualization_utils import STANDARD_COLORS, draw_mask_on_image_array, \
+    draw_bounding_box_on_image_array
+
+from utils.drawing import draw_text_pil, draw_axis
+
+
+def pil_to_cv_image(image_pil, output_bgr=True):
+    image_cv = np.array(image_pil)
+    if output_bgr:
+        image_cv = image_cv[:, :, ::-1]
+    return image_cv
+
+
+def cv_to_pil_image(image_cv, output_rgb=True):
+    if output_rgb:
+        image_cv = image_cv[..., ::-1]  # Convert BGR opencv image to RGB first
+    return Image.fromarray(image_cv)
+
+
+def draw_bounding_box_pil(box, draw, color, image_np):
+    ymin = box[0]
+    xmin = box[1]
+    ymax = box[2]
+    xmax = box[3]
+    im_width, im_height = image_np.shape[1], image_np.shape[0]
+    (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                  ymin * im_height, ymax * im_height)
+    draw.line([(left, top), (left, bottom), (right, bottom),
+               (right, top), (left, top)], width=6, fill=color)
+
+
+def draw_match_boxes_with_same_color(image_np, face_boxes, person_boxes, matched):
+    image_pil = Image.fromarray(image_np)
+    draw = ImageDraw.Draw(image_pil)
+
+    i = 0
+    for f, p in zip(face_boxes[0][matched[:, 0]], person_boxes[matched[:, 1]]):
+        # bbox_color = (255, 0, 0) if people[id].gender == 0 else (0, 0, 255)
+        i += 1
+        bbox_color = STANDARD_COLORS[
+            i % len(STANDARD_COLORS)]
+
+        draw_bounding_box_pil(f, draw, bbox_color, image_np)
+        draw_bounding_box_pil(p, draw, bbox_color, image_np)
+
+    image_np = np.array(image_pil)
+    return image_np
+
+
+male_text = u" Male;  "
+female_text = u" Female;  "
+unspecified_text = u" Unspecified;  "
+facemask_text = u"Face mask;  "
+formal_text = u"Formal;  "
+hat_text = u"Hat;  "
+jeans_text = u"Jeans;  "
+logo_text = u"Logo;  "
+longhair_text = u"Long Hair;  "
+longpants_text = u"Long pants;  "
+longsleeve_text = u"Long sleeve;  "
+shorts_text = u"Shorts;  "
+skirt_text = u"Skirt;  "
+stripe_text = u"Stripe;  "
+sunglass_text = u"Sunglasses;  "
+tshirt_text = u"T-shirt;  "
+
+def draw_tracked_people(img_bgr, tracked_people):
+    """
+    Draw bounding box and mask of detected and tracked people, each with a different color based on their ids.
+    :param img_bgr: Original image where people are detected and tracked.
+    :param tracked_people: a list of TrackedPerson objects.
+    :return A numpy
+    """
+    if len(tracked_people) == 0:
+        return img_bgr
+
+    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+    # img_pil = Image.fromarray(img_rgb)
+
+    for person in tracked_people:
+        color = STANDARD_COLORS[np.random.randint(1, 100) % len(STANDARD_COLORS)]
+        text = label_to_text(person.male)
+
+        if person.facemask == 1:
+            text += facemask_text
+        if person.formal == 1:
+            text += formal_text
+        if person.hat == 1:
+            text += hat_text
+        if person.jeans == 1:
+            text += jeans_text
+        if person.logo == 1:
+            text += logo_text
+        if person.longhair == 1:
+            text += longhair_text
+        if person.longpants == 1:
+            text += longpants_text
+        if person.longsleeve == 1:
+            text += longsleeve_text
+        if person.shorts == 1:
+            text += shorts_text
+        if person.skirt == 1:
+            text += skirt_text
+        if person.stripe == 1:
+            text += stripe_text
+        if person.sunglass == 1:
+            text += sunglass_text
+        if person.tshirt == 1:
+            text += tshirt_text
+
+
+
+        if person.box is not None:
+            xmin, ymin, xmax, ymax = person.box
+            draw_bounding_box_on_image_array(img_rgb, ymin, xmin, ymax, xmax, color=color, thickness=2,
+                                             display_str_list=[text[:-3]],
+                                             use_normalized_coordinates=False)
+        # if person.face_box is not None:
+        #     xmin, ymin, xmax, ymax = person.face_box
+        #     draw_bounding_box_on_image_array(img_rgb, ymin, xmin, ymax, xmax, color=color, thickness=2,
+        #                                      display_str_list=['Score:{:.2f}'.format(person.face_score)],
+        #                                      use_normalized_coordinates=False)
+        # if person.mask is not None:
+        #     draw_mask_on_image_array(img_rgb, person.mask, color=color)
+
+    return cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
+
+
+
+def label_to_text(label):
+    if label == 1:
+        return male_text
+    elif label == 0:
+        return female_text
+    else:
+        return unspecified_text
